c1d84e3cd8b40547da610e653829318a28bf8d7a
Emrah Gunduz <emrah.gunduz@markakod.com>
2017-08-23 10:39:49 +0300

    Debug for commands

--- utils/network.js
+++ utils/network.js
@@ -22,10 +22,11 @@ Network.prototype.__createIpSetList = function ( from ) {
   ];
 
   for ( var m = 0; m < commands.length; m++ ) {
+    global.logger( from, "Running " + commands[ m ] );
     that.__exec( commands[ m ], function ( error ) {
       if ( error ) {
         if ( !global.DEBUG ) {
-          global.logger( from, "ERROR: Could not write to ipset table" );
+          global.logger( from, "ERROR: Could not execute command" );
         }
       }
     } );
